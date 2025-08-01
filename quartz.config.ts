af348952c8c265a9ba6828e2d1266a0bfa51a38d
Jeppe Lynge Hansen <jeppeklh@hotmail.com>
2025-08-01 19:20:07 +0200

    feat: initial migration

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,15 +8,15 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
-    pageTitleSuffix: "",
+    pageTitle: "Datamatiker Noter",
+    pageTitleSuffix: " | Jeppe Lynge Hansen",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "https://jeppeklh.github.io/Datamatiker-noter-quartz/",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
